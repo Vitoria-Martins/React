eed843b41c6b5ea1e473cdc6a4773875f963af1e
vitoria299 <92556441+vitoria299@users.noreply.github.com>
2023-03-08 17:23:30 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-//define um componente react
 export default function App(){
-     return(
-      <div>
-        <h1>Bem cindo ao sistema</h1>
-      </div>
-     )
+  return(
+    <div>
+      <h1>Bem vindo ao sistema</h1>
+    </div>
+  )
 }
