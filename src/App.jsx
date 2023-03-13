860366253d92e8e5fcfde7555796cec787984b54
vitoria299 <92556441+vitoria299@users.noreply.github.com>
2023-03-13 17:04:27 -0300

    states

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
-import React from "react";
+//ciclo de vida do componente
+
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Meu projeto</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
